0a883176a8625cce46eeaf0409c09a3b02a24382
Mayank Sinha <mayank06sinha@gmail.com>
2016-10-31 18:19:35 +0530

    heroku deployment

--- src/main/java/com/mayankpractice/spring/config/AppConfig.java
+++ src/main/java/com/mayankpractice/spring/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -25,6 +26,10 @@ public class AppConfig extends WebMvcConfigurerAdapter{
 		return viewResolver;
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
--- src/main/java/com/mayankpractice/spring/controller/HelloController.java
+++ src/main/java/com/mayankpractice/spring/controller/HelloController.java
@@ -1,8 +1,12 @@
 package com.mayankpractice.spring.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -12,6 +16,13 @@ public class HelloController {
 
 		model.addAttribute("name", "Mayank Sinha");
 
-		return "welcome";
+		return "index";
+	}
+	
+	@RequestMapping("/index")
+	public ModelAndView index(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		return new ModelAndView("/resources/index.html");
 	}
 }
